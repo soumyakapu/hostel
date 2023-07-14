cf4301f3776f67fe809d99a53afb30e0a9a64b79
soumyakapu <Soumyakapu@gmail.co>
2023-07-14 17:33:49 +0530

    added jwt service

---
+++ src/main/java/com/app/service/JwtService.java
@@ -0,0 +1,35 @@
+package com.app.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class JwtService {
+    public static final String  SECRET = "SecretkEY";
+    public String  generateToken(String email){
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims,email);
+    }
+
+    private String createToken(Map<String, Object> claims, String email) {
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(email)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() +1000*60*30))
+                .signWith(getSignKey(),SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
